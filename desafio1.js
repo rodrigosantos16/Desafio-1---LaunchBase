6769ba52f2bc55b5a83e6e9c6cf9030b326a6e2d
Rodrigo Santos <rod.santos014@gmail.com>
2020-06-15 20:37:17 -0300

    Add files via upload

---
+++ desafio1.js
@@ -0,0 +1,12 @@
+const nome = 'Rodrigo'
+const peso = 58
+const altura = 1.72
+
+const imc = peso / (altura * altura)
+
+    if(imc >= 30){
+        console.log('Você está acima do peso')
+    }else{
+        console.log('Você não está acima do peso')
+    }
+
